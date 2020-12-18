7db6b16c8ba16ba389b99340d6157c6420b6368f
dsrules007 <sousilvaney@gmail.com>
2020-12-18 14:33:31 +0000

    remove files and fix styles

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -68,58 +68,60 @@ export default function Details(props) {
             err = true;
         })
 
-    if (err) {
-        await youTube(movieProps.title).get().then(result => {
-            if(result.data.items[0].id.videoId){
-            trailer = result.data.items[0].id.videoId;
-            }else{
-                trailer = "Zw_FKq10S8M";
-            }
+        if (err) {
+            await youTube(movieProps.title).get().then(result => {
+                if (result.data.items[0].id.videoId) {
+                    trailer = result.data.items[0].id.videoId;
+                } else {
+                    trailer = "Zw_FKq10S8M";
+                }
 
-        }).catch(log => {
-            trailer = "Zw_FKq10S8M";
-        })
+            }).catch(log => {
+                trailer = "Zw_FKq10S8M";
+            })
+        }
+
+        return trailer;
     }
 
-    return trailer;
-}
 
 
-
-return (
-    <>
-        <div className="detail">
-            <div className='detail-container'>
-                <div className='detail-img'>
-                    {movie.details.poster_path ?
-                        <img src={`https://image.tmdb.org/t/p/w500${movie.details.poster_path}`} alt='img'></img> :
-                        <img src={auxImg} alt='img' width="100%" height="100%" overflow="hidden" background="none"></img>}
-                </div>
-                <div className='detail-info'>
-                    <div className="detail-video" >
-                        <iframe src={`https://www.youtube.com/embed/${movie.trailer}`}
-                            frameBorder="0"
-                            allowFullScreen>
-                        </iframe>
+    return (
+        <>
+            <div className="detail">
+                <div className='detail-container'>
+                    <div className='detail-img'>
+                        {movie.details.poster_path ?
+                            <img src={`https://image.tmdb.org/t/p/w500${movie.details.poster_path}`} alt='img'></img> :
+                            <img src={auxImg} alt='img' width="100%" height="100%" overflow="hidden" background="none"></img>}
                     </div>
-                    <div className='detail-about'>
-                        <div className="about-1">
-                            <h1 className="title"><strong>{movie.details.title}</strong></h1><br />
-                            <p className="overview">{movie.details.overview}</p>
+                    <div className='detail-info'>
+                        <div className="detail-video" >
+                            <iframe src={`https://www.youtube.com/embed/${movie.trailer}`}
+                                frameBorder="0"
+                                allowFullScreen>
+                            </iframe>
                         </div>
-                        <div className="about-2">
-                            <p className="classification"> <Classification className="icon1" /> <span> {movie.details.vote_average} </span></p>
-                            <p className="genre"><Genre className="icon2" />  <span> {movie.genres.map(genre => `${genre.name} `)} </span></p>
-                            <p className="release"> <Release className="icon2" /> <span> {movie.details.release_date} </span></p>
-                            <p className="country"> <Country className="icon2" /> <span> {movie.countries.map(cod =>
-                                <img src={`https://www.countryflags.io/${cod.iso_3166_1}/shiny/24.png`} alt={cod.iso_3166_1} ></img>)}</span> </p>
-                            <p className="idiom"> <Idiom className="icon2" /> <span> {movie.idiom.map(cod =>
-                                <img src={`https://unpkg.com/language-icons/icons/${cod.iso_639_1}.svg`} alt={cod.iso_639_1} width="19px" height="19px"></img>)}</span> </p>
+                        <div className='detail-about'>
+                            <div className="about-1">
+                                <div className="about-1-info">
+                                    <h1 className="title">{movie.details.title}</h1>
+                                    <p className="overview">{movie.details.overview}</p>
+                                </div>
+                            </div>
+                            <div className="about-2">
+                                <p className="classification"> <Classification className="icon1" /> <span> {movie.details.vote_average} </span></p>
+                                <p className="genre"><Genre className="icon2" />  <span> {movie.genres.map(genre => `${genre.name} `)} </span></p>
+                                <p className="release"> <Release className="icon2" /> <span> {movie.details.release_date} </span></p>
+                                <p className="country"> <Country className="icon2" /> <span> {movie.countries.map(cod =>
+                                    <img src={`https://www.countryflags.io/${cod.iso_3166_1}/shiny/24.png`} alt={cod.iso_3166_1} ></img>)}</span> </p>
+                                <p className="idiom"> <Idiom className="icon2" /> <span> {movie.idiom.map(cod =>
+                                    <img src={`https://unpkg.com/language-icons/icons/${cod.iso_639_1}.svg`} alt={cod.iso_639_1} width="19px" height="19px"></img>)}</span> </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
-)
+        </>
+    )
 }
